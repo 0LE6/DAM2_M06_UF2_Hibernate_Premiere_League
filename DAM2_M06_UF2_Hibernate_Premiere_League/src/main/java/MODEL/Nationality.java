5a7575bda831efd18ce5d5296124dd643f3bb33a
Oleg <kharenkoleg@gmail.com>
2023-12-04 17:01:52 +0100

    empezando un poco con el 3.5

---
+++ DAM2_M06_UF2_Hibernate_Premiere_League/src/main/java/MODEL/Nationality.java
@@ -0,0 +1,26 @@
+package MODEL;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="nations")
+public class Nationality implements Serializable {
+
+	private static final long serialVersionUID = 4L;
+	
+	@Id
+	@Column(name="abv")
+	private String abv;
+	
+	@Id
+	@Column(name="description")
+	private String description;
+	
+	
+
+}
