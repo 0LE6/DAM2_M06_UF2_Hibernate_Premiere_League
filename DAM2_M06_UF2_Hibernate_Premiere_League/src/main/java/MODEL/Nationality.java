5f7157fd6789f1023d8ee805e4cc27af9e04fd2c
Oleg <kharenkoleg@gmail.com>
2023-12-04 18:27:14 +0100

    empezamos con este side project sobre el A3.5

--- DAM2_M06_UF2_Hibernate_Premiere_League/src/main/java/MODEL/Nationality.java
+++ DAM2_M06_UF2_Hibernate_Premiere_League/src/main/java/MODEL/Nationality.java
@@ -1,24 +1,24 @@
-//package MODEL;
-//
-//import java.io.Serializable;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name="nations")
-//public class Nationality implements Serializable {
-//
-//	private static final long serialVersionUID = 4L;
-//	
-//	@Id
-//	@Column(name="abv")
-//	private String abv;
-//
-//	@Column(name="description")
-//	private String description;
-//	
-//
-//}
+package MODEL;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="nations")
+public class Nationality implements Serializable {
+
+	private static final long serialVersionUID = 4L;
+	
+	@Id
+	@Column(name="abv")
+	private String abv;
+
+	@Column(name="description")
+	private String description;
+	
+
+}
