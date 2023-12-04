4748dd8657e5c4ed58dde879381afc338be015a2
Oleg <kharenkoleg@gmail.com>
2023-12-04 18:56:54 +0100

    terminando la clase Nationality

--- DAM2_M06_UF2_Hibernate_Premiere_League/src/main/java/MODEL/Nationality.java
+++ DAM2_M06_UF2_Hibernate_Premiere_League/src/main/java/MODEL/Nationality.java
@@ -1,6 +1,7 @@
 package MODEL;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,13 +13,56 @@ import javax.persistence.Table;
 public class Nationality implements Serializable {
 
 	private static final long serialVersionUID = 4L;
-	
+	 
 	@Id
 	@Column(name="abv")
 	private String abv;
 
 	@Column(name="description")
 	private String description;
-	
 
+	public Nationality(String abv, String description) {
+		super();
+		this.abv = abv;
+		this.description = description;
+	}
+
+	public String getAbv() {
+		return abv;
+	}
+
+	public void setAbv(String abv) {
+		this.abv = abv;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(abv, description);
+	} 
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Nationality other = (Nationality) obj;
+		return Objects.equals(abv, other.abv) && Objects.equals(description, other.description);
+	}
+	
+	
 }
